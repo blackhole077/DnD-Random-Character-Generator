aac5655e13205a79cf26f99206a1d920b3440711
blackhole077 <rajagopal.jeevan@gmail.com>
2018-07-20 17:20:53 -0500

    Updated Character header file to match src file

--- headers/Character.h
+++ headers/Character.h
@@ -9,14 +9,17 @@ class Character{
 private:
     Race race;
     Stats stats;
+    int gender;
     int effective_level;
     int total_hitpoints;
 public:
     Character(Race race, Stats stats);
-    
+
+    void set_gender(int gender);
     void set_effective_level(int level);
     void set_total_hitpoints(int hp);
 
+    int get_gender() const;
     int get_effective_level() const;
     int get_total_hitpoints() const;
     Race get_race() const;
