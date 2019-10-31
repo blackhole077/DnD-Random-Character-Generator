8e22bbbaf45d1cd7e22bd174840486d6928900d0
blackhole077 <rajagopal.jeevan@gmail.com>
2019-10-30 22:39:28 -0500

    Created small driver function and wrote code to read in file for testing

---
+++ drivers/skills_driver.cpp
@@ -0,0 +1,11 @@
+#include "..\headers\Stats.h"
+#include "..\headers\dnd_template_functions.h"
+using namespace std;
+
+int main(){
+  cout << "Hello there!" << endl;
+  Skills all_skills;
+  int error_code = initialize_all_skills();
+
+  return 0;
+}
--- src/Skills.cpp
+++ src/Skills.cpp
@@ -11,11 +11,20 @@ Skills::Skills(){
 }
 
 int Skills::initialize_all_skills(){
-  this->num_skills = 45;
-  this->num_cols = 4;
+  // this->num_skills = 45;
+  // this->num_cols = 4;
   this->skills_ranks_and_bonuses = malloc(sizeof(int) * (num_skills * num_cols));
   ifstream file_data;
   file_data.open("..\\data\\skills_list.txt");
+  /**Fetch the first line, which gets the number of skills and number of columns **/
+  std::string line;
+  std::string line_chunk;
+  getline(file_data, line);
+  this->num_skills = stoi(getline(line, line_chunk, ","),nullptr,10);
+  this->num_cols = stoi(getline(line, line_chunk, ","),nullptr,10);
+  std::cout << "Number of skills: " << this->num_skills << endl;
+  std::cout << "Number of cols: " << this->num_skills << endl;
 
+  file_data.close();
   return 0;
 }
